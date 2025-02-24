96ef86226b1c00ad619f9d38ff98db4790306184
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-02-24 14:20:23 -0500

    It now actually sends a simple themed Email

--- MyNewsletter/MainWindow.xaml.cs
+++ MyNewsletter/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +11,9 @@ using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MimeKit;
+using MimeKit.Utils;
+using MyNewsletter.Tools;
 
 namespace MyNewsletter
 {
@@ -74,6 +79,79 @@ namespace MyNewsletter
                 SendToBox.Text = "";
                 SendToBox.Foreground = Brushes.Black;
                 DefaultTextBox = false;
+            }
+        }
+
+        private void SendButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (EmailSender.IsValidEmail(SendToBox.Text))
+            {
+                if (SelectedTheme != Brushes.GhostWhite)
+                {
+                    bool EmailSent = false;
+                    if (SelectedTheme == Brushes.HotPink)
+                    {
+                        BodyBuilder body = new BodyBuilder();
+                        string mainDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+                        var imagePath = mainDirectory + "/Images/BirthdayBack.jpg";
+                        var image = body.LinkedResources.Add(imagePath);
+                        image.ContentId = MimeUtils.GenerateMessageId(); // Generate a unique Content-ID
+
+
+                        body.HtmlBody = 
+                            $"<div style=\" width: 600px; height: 350px; background-color: lightblue; background-image: url(cid:{image.ContentId}); background-repeat: no-repeat; background-size: cover;\">" +
+                                $"\r<h1 style=\"display: flex; justify-content: center; align-items: center; padding: 20px; text-shadow: -1px -1px 0px White, 1px -1px 0px White, -1px 1px 0px White, 1px 1px 0px White; color: #ffd800\">Happy Birthday {SendToBox.Text}!</h1>" +
+                            $"\r</div>";
+
+
+                        EmailSender.SendEmail("User", SendToBox.Text, "Happy Birthday!", body);
+                        EmailSent = true;
+                    }
+                    else if (SelectedTheme == Brushes.ForestGreen)
+                    {
+                        BodyBuilder body = new BodyBuilder();
+
+                        body.HtmlBody =
+                            $"<div style=\" width: 600px; height: 350px; background-color: ForestGreen; background-repeat: no-repeat; background-size: cover;\">" +
+                                $"\r<h1 style=\"display: flex; justify-content: center; align-items: center; padding: 20px; text-shadow: -1px -1px 0px White, 1px -1px 0px White, -1px 1px 0px White, 1px 1px 0px White; color: #ffd800\">{SendToBox.Text}, Don't Miss the new Sale!</h1>" +
+                            $"\r</div>";
+
+                        EmailSender.SendEmail("User", SendToBox.Text, "Sale", body);
+                        EmailSent = true;
+                    }
+                    else if (SelectedTheme == Brushes.DarkGoldenrod)
+                    {
+                        BodyBuilder body = new BodyBuilder();
+                        string mainDirectory = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+                        var imagePath = mainDirectory + "/Images/Wildfire.jpg";
+                        var image = body.LinkedResources.Add(imagePath);
+                        image.ContentId = MimeUtils.GenerateMessageId(); // Generate a unique Content-ID
+
+
+                        body.HtmlBody =
+                            $"<div style=\" width: 600px; height: 350px; background-color: ForestGreen; background-image: url(cid:{image.ContentId}); background-repeat: no-repeat; background-size: cover;\">" +
+                                $"\r<h1 style=\"display: flex; justify-content: center; align-items: center; padding: 20px; text-shadow: -1px -1px 0px White, 1px -1px 0px White, -1px 1px 0px White, 1px 1px 0px White; color: #ffd800\">{SendToBox.Text}, Wildfires broke out near you!</h1>" +
+                            $"\r</div>";
+                        EmailSender.SendEmail("User", SendToBox.Text, "NewsLetter", body);
+                        EmailSent = true;
+                    }
+                    if (EmailSent == true)
+                    {
+                        MessageBox.Show("Email Has Been Sent Successfully!", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Something went wrong on our end... We are sorry.", "Failure", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    TypeChoiceError.Text = "Please pick what you would like to send!";
+                }
+            }
+            else
+            {
+                EmailError.Text = "Invalid Email!";
             }
         }
     }
---
+++ MyNewsletter/Tools/EmailSender.cs
@@ -0,0 +1,71 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+//using System.Net.Mail;
+
+namespace MyNewsletter.Tools
+{
+    class EmailSender
+    {
+        public async static void SendEmail(string From, string To, string Subject, string Body)
+        {
+            await Task.Run(() =>
+            {
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(From, "mytempnewsletter@gmail.com"));
+                message.To.Add(new MailboxAddress("Name", To));
+                message.Subject = Subject;
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = Body
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate("mytempnewsletter@gmail.com", "mafg yqtw dbto sonf"); // Passord: #1EmailSender
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            });
+        }
+        public async static void SendEmail(string From, string To, string Subject, BodyBuilder Body)
+        {
+            await Task.Run(() =>
+            {
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(From, "mytempnewsletter@gmail.com"));
+                message.To.Add(new MailboxAddress("Name", To));
+                message.Subject = Subject;
+
+                message.Body = Body.ToMessageBody();
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate("mytempnewsletter@gmail.com", "mafg yqtw dbto sonf");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            });
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
